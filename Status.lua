470a369b76c5c9b0cb8093d39ee10160fcdbc8e7
binary <126812477+therealbinary@users.noreply.github.com>
2023-03-14 19:00:22 +0100

    Update Status.lua

--- Status.lua
+++ Status.lua
@@ -1,7 +1,7 @@
 return {
     [8540346411] = {
       ScriptDown = {false, "res"},
-      ScriptVer = "0.8.3",
+      ScriptVer = "0.8.4",
       Codes = {"Clover", "dragon", "jungle", "anniversary", "100m", "flower", "molten", "ocean", "fish", "2023", "santa", "christmas", "exotic", "95m", "pirate", "ancient", "seacave", "90m", "evil", "halloween", "update32", "axolotls", "aqua", "blackhole", "hacker", "saturn", "sun", "destruction", "175kthanks", "tutel", "shadow", "neon", "75m", "lab", "fantasy", "kraken", "summer", "150kthanks", "60m", "galactic", "alien", "mars", "space", "50m", "hell", "steampunk", "100kthanks", "75kthanks", "thanksfor50k", "10mthanks", "wow30000", "magic", "20kthanku", "freeluckboost", "wow10klikesthanks", "freeclicksomg", "5klikesthanks", "wow2500likes", "already1500likes", "thanks500likes", "RELEASE"};
     },
     [11746859781] = {
