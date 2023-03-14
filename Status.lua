f5feb3f70b899435875a1bbca4261571fca5f4b6
binary <126812477+therealbinary@users.noreply.github.com>
2023-03-14 20:55:11 +0100

    Update Status.lua

--- Status.lua
+++ Status.lua
@@ -9,5 +9,9 @@ return {
       ScriptVer = "0.0.1 BETA",
       Codes = {"underthesea", "spongebob", "gofast", "secrets", "season1", "bubblegum", "banana", "bandana", "nana", "OPE", "scramble", "stayfrosty", "lucky", "happynewyear", "2022", "OmgSanta", "Rudolph", "Release"};
     },
+    [621129760] = {
+      ScriptDown = {false, "res"},
+      ScriptVer = "0.0.1 BETA";
+    },
     DiscordServer = "EaDV7sCPnj"
 }
