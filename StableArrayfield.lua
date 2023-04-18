1ebfa86400aa67576fef929c89416913599ca549
binary <126812477+therealbinary@users.noreply.github.com>
2023-04-18 23:10:45 +0200

    Update StableArrayfield.lua

--- StableArrayfield.lua
+++ StableArrayfield.lua
@@ -1373,13 +1373,6 @@ function RayfieldLibrary:CreateWindow(Settings)
 	TweenService:Create(Main.SideTabList, TweenInfo.new(0, Enum.EasingStyle.Quint), {BackgroundTransparency = 1,Size = UDim2.new(0,150,0,390),Position = UDim2.new(0,10,0.5,22)}):Play()
 	TweenService:Create(Main.SideTabList.UIStroke, TweenInfo.new(0, Enum.EasingStyle.Quint),{Transparency = 1}):Play()
 	TweenService:Create(Main.SideTabList.RDMT, TweenInfo.new(0, Enum.EasingStyle.Quint),{TextTransparency = 1}):Play()
-	delay(4,function()
-		qNotePrompt({
-			Title = 'New Library Documentation Available',
-			Description = 'The Library Developer has updated the Documentation and recommends all script developers to take a look.',
-
-		})
-	end)
 	TopList.Template.Visible = false
 	SideList.SideTemplate.Visible = false
 	Notifications.Template.Visible = false
