8d44229d69b9b5ec737efdde1d1a205090c996cc
binary <126812477+therealbinary@users.noreply.github.com>
2023-04-23 20:41:23 +0200

    Update Status.lua

--- Status.lua
+++ Status.lua
@@ -1,17 +1,8 @@
 return {
-    [8540346411] = {
-      ScriptDown = {false, "res"},
-      ScriptVer = "0.8.4",
-      Codes = {"Clover", "dragon", "jungle", "anniversary", "100m", "flower", "molten", "ocean", "fish", "2023", "santa", "christmas", "exotic", "95m", "pirate", "ancient", "seacave", "90m", "evil", "halloween", "update32", "axolotls", "aqua", "blackhole", "hacker", "saturn", "sun", "destruction", "175kthanks", "tutel", "shadow", "neon", "75m", "lab", "fantasy", "kraken", "summer", "150kthanks", "60m", "galactic", "alien", "mars", "space", "50m", "hell", "steampunk", "100kthanks", "75kthanks", "thanksfor50k", "10mthanks", "wow30000", "magic", "20kthanku", "freeluckboost", "wow10klikesthanks", "freeclicksomg", "5klikesthanks", "wow2500likes", "already1500likes", "thanks500likes", "RELEASE"};
-    },
     [11746859781] = {
       ScriptDown = {false, "res"},
       ScriptVer = "0.0.1 BETA",
       Codes = {"underthesea", "spongebob", "gofast", "secrets", "season1", "bubblegum", "banana", "bandana", "nana", "OPE", "scramble", "stayfrosty", "lucky", "happynewyear", "2022", "OmgSanta", "Rudolph", "Release"};
-    },
-    [621129760] = {
-      ScriptDown = {false, "res"},
-      ScriptVer = "0.0.1 BETA";
-    },
+    }
     DiscordServer = "EaDV7sCPnj"
 }
